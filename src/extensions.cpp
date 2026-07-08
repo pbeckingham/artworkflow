cfdaec619b6b59b9400a57dbd82fe26aeea7152e
Paul Beckingham <paul@beckingham.net>
2026-07-08 12:30:09 -0400

    Added stub files for extension callouts

---
+++ src/extensions.cpp
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2026, Paul Beckingham.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// https://opensource.org/license/mit
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <format.h>
+#include <shared.h>
+#include <iostream>
+#include <artworkflow.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// List of C functions accessible to Lua:
+//   apiGetVersion         Returns product version string
+//   apiDomGet             Returns DOM value
+//
+// List of functions to override in Lua:
+//   extensionOnEntry      Earliest opportunity to run
+//   extensionOnExit       Earliest opportunity to run
+//   extensionCmdVersion   Returns additional version text
+//   extensionCmdHelp      Returns additional help text
+//
+////////////////////////////////////////////////////////////////////////////////
