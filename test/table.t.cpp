6987d068b00f0f3f7b9530327ad05f5da4c52e72
Paul Beckingham <paul@beckingham.net>
2026-07-08 20:37:26 -0400

    Fixed spacing in table cell for wrapping

--- test/table.t.cpp
+++ test/table.t.cpp
@@ -67,8 +67,8 @@ int main (int, char**)
     t1.set (row, 0, "bottom left", single_cell);
     t1.set (row, 1, "bottom center, containing sufficient text that "
                              "wrapping will occur because it exceeds all "
-                             "reasonable values for default width.  Even in a "
-                             "very wide terminal window.  Just look at the "
+                             "reasonable values for default width. Even in a "
+                             "very wide terminal window. Just look at the "
                              "lengths we must go to, to get passing unit tests "
                              "and not flaky tests.");
     t1.set (row, 2, "bottom right");
