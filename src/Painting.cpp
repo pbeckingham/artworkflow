d304a1c9cb2d7037c7e7995a6b721fb25656cbcf
Paul Beckingham <paul@beckingham.net>
2026-07-28 09:16:08 -0400

    Fixed utf8 parsing issue

--- src/Painting.cpp
+++ src/Painting.cpp
@@ -34,6 +34,7 @@
 #include <Lexer.h>
 #include <Color.h>
 #include <Composite.h>
+#include <utf8.h>
 #include <Painting.h>
 #include <artworkflow.h>
 
@@ -672,53 +673,53 @@ bool Painting::is_painting (const std::string& sample)
 void Painting::parse (const std::string& line)
 {
   if (line.length () > 1)
-    _id         = Lexer::trimRight (line.substr (1, 4));
+    _id         = Lexer::trimRight (utf8_substr (line, 1, 4));
   else
     throw "Missing id to parse.";
 
   if (line.length () >  7)
-    _title      = Lexer::trimRight (line.substr (6, 28));
+    _title      = Lexer::trimRight (utf8_substr (line, 6, 28));
   else
     throw "Missing title to parse.";
 
   if (line.length () > 35)
-    _series     = Lexer::trimRight (line.substr (34, 3));
+    _series     = Lexer::trimRight (utf8_substr (line, 34, 3));
 
   if (line.length () > 39)
-    _start      = Lexer::trimRight (line.substr (38, 11));
+    _start      = Lexer::trimRight (utf8_substr (line, 38, 11));
 
   if (line.length () > 51)
-    _end        = Lexer::trimRight (line.substr (50, 11));
+    _end        = Lexer::trimRight (utf8_substr (line, 50, 11));
 
   if (line.length () > 63)
-    _varnish    = Lexer::trimRight (line.substr (62, 11));
+    _varnish    = Lexer::trimRight (utf8_substr (line, 62, 11));
 
   if (line.length () > 75)
-    _action     = Lexer::trimRight (line.substr (74, 11));
+    _action     = Lexer::trimRight (utf8_substr (line, 74, 11));
 
   if (line.length () > 87)
-    _size       = Lexer::trimRight (line.substr (86, 5));
+    _size       = Lexer::trimRight (utf8_substr (line, 86, 5));
 
   if (line.length () > 93)
-    _substrate  = Lexer::trimRight (line.substr (92, 3));
+    _substrate  = Lexer::trimRight (utf8_substr (line, 92, 3));
 
   if (line.length () > 97)
-    _tagged     = Lexer::trimRight (line.substr (96, 1));
+    _tagged     = Lexer::trimRight (utf8_substr (line, 96, 1));
 
   if (line.length () > 99)
-    _varnished  = Lexer::trimRight (line.substr (98, 1));
+    _varnished  = Lexer::trimRight (utf8_substr (line, 98, 1));
 
   if (line.length () > 101)
-    _archived   = Lexer::trimRight (line.substr (100, 1));
+    _archived   = Lexer::trimRight (utf8_substr (line, 100, 1));
 
   if (line.length () > 106)
-    _www        = Lexer::trimRight (line.substr (105, 1));
+    _www        = Lexer::trimRight (utf8_substr (line, 105, 1));
 
   if (line.length () > 108)
-    _complexity = Lexer::trimRight (line.substr (107, 2));
+    _complexity = Lexer::trimRight (utf8_substr (line, 107, 2));
 
   if (line.length () > 111)
-    _notes      = Lexer::trimRight (line.substr (110), " \n");
+    _notes      = Lexer::trimRight (utf8_substr (line, 110), " \n");
 
   _height = _width = 0;
   if (_size != "")
